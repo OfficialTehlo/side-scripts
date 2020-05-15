b106064d7a56624b473c7dd6739e8aee4971ca02
Tehlo <51698749+TehloWasTaken@users.noreply.github.com>
2020-05-15 13:54:23 +0200

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -1,22 +1,24 @@
-echo "What is the domain + subdomain you want to use?"
-read ccdomain
 
-echo "What is the domain or IP address you want to proxy? (Service IP and Port)"
-read ccip
-
-echo " server {
-        server_name $ccdomain;
-        listen 80;
-        listen [::]:80;
-        access_log /var/log/nginx/reverse-access.log;
-        error_log /var/log/nginx/reverse-error.log;
-        location / {
-                    proxy_pass https://$ccip;
-  }
-} " > /etc/nginx/sites-available/$ccdomain.conf
-
-ln -s /etc/nginx/sites-available/$ccdomain.conf /etc/nginx/sites-enabled/$ccdomain.conf
-
-certbot --nginx
-
-echo "Done! :)"
+                                                                                                                                                                  
+echo "What is the domain name you want to use? (subdomain + domain)"                                                                                                                   
+read ccdomain                                                                                                                                                     
+                                                                                                                                                                  
+echo "What is the domain or IP address you want to proxy? (IP and port)"                                                                                   
+read ccip                                                                                                                                                         
+                                                                                                                                                                  
+echo " server {                                                                                                                                                   
+        server_name $ccdomain;                                                                                                                                    
+        listen 80;                                                                                                                                                
+        listen [::]:80;                                                                                                                                           
+        access_log /var/log/nginx/reverse-access.log;                                                                                                             
+        error_log /var/log/nginx/reverse-error.log;                                                                                                               
+        location / {                                                                                                                                              
+                    proxy_pass http://$ccip;                                                                                                                      
+  }                                                                                                                                                               
+} " > /etc/nginx/sites-available/$ccdomain.conf                                                                                                                   
+                                                                                                                                                                  
+ln -s /etc/nginx/sites-available/$ccdomain.conf /etc/nginx/sites-enabled/$ccdomain.conf                                                                           
+                                                                                                                                                                  
+certbot --nginx                                                                                                                                                   
+                                                                                                                                                                  
+echo "Done! :)"   
