949619f142987541dcf3013ab58739a30c1c9044
Tehlo <51698749+TehloWasTaken@users.noreply.github.com>
2021-10-03 20:28:40 +0200

    Update wss-proxy.sh

--- wss-proxy.sh
+++ wss-proxy.sh
@@ -5,21 +5,26 @@ echo "What is the domain or IP address you want to proxy? (IP and port)"
 read ccip                                                                                                                                                         
                                                                                                                                                                   
 echo " 
-        map $http_upgrade $connection_upgrade {
-        default upgrade;
-        '' close;
-        }
-        upstream websocket {
-        server $ccip;
-        }
-        server {                                                                                                                                                   
-        server_name $ccdomain;                                                                                                                                    
-        listen 80;                                                                                                                                                
-        listen [::]:80;                                                                                                                                           
-        access_log /var/log/nginx/reverse-access.log;                                                                                                             
-        error_log /var/log/nginx/reverse-error.log;                                                                                                               
-        location / {                                                                                                                                              
-                    proxy_pass http://$ccip;                                                                                                                      
+map $http_upgrade $connection_upgrade {
+  default upgrade;
+  '' close;
+}
+upstream websocket {
+   server $ccip;
+}
+server {                                                                                                                                                   
+   server_name $ccdomain;                                                                                                                                    
+   listen 80;                                                                                                                                                
+   listen [::]:80;                                                                                                                                           
+      access_log /var/log/nginx/reverse-access.log;                                                                                                             
+      error_log /var/log/nginx/reverse-error.log;                                                                                                               
+   location / {
+      proxy_pass http://websocket;
+      proxy_http_version 1.1;
+      proxy_set_header Upgrade $http_upgrade;
+      proxy_set_header Connection $connection_upgrade;
+    }
+                                                                                                                   
   }                                                                                                                                                               
 } " > /etc/nginx/sites-available/$ccdomain.conf                                                                                                                   
                                                                                                                                                                   
